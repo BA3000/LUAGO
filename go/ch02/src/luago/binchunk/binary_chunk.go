e0769fb00562cb1cd0b53b43e321ef842fc5d349
BA3000 <2864281096@qq.com>
2021-10-18 14:20:58 +0800

    ch02 (not finished

---
+++ go/ch02/src/luago/binchunk/binary_chunk.go
@@ -0,0 +1,21 @@
+package binchunk
+
+type binaryChunk struct {
+	header                  // 头部
+	sizeUpvalues byte       // 主函数upvalue数量
+	mainFunc     *Prototype // 主函数原型
+}
+
+type header struct {
+	signature       [4]byte
+	version         byte
+	format          byte
+	luacData        [6]byte
+	cintSize        byte
+	sizetSize       byte
+	instructionSize byte
+	luaIntegerSize  byte
+	luaNumberSize   byte
+	luacInt         int64
+	luacNum         float64
+}
