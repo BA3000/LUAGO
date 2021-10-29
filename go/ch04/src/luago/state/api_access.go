12f85eaff5daa37bc9a59e5f58cec56d2271b57f
BA3000 <2864281096@qq.com>
2021-10-29 13:23:00 +0800

    finish ch4

--- go/ch04/src/luago/state/api_access.go
+++ go/ch04/src/luago/state/api_access.go
@@ -56,6 +56,18 @@ func (lState *luaState) IsBoolean(idx int) bool {
 	return lState.Type(idx) == LUA_TBOOLEAN
 }
 
+func (lState *luaState) IsTable(idx int) bool {
+	return lState.Type(idx) == LUA_TTABLE
+}
+
+func (lState *luaState) IsFunction(idx int) bool {
+	return lState.Type(idx) == LUA_TFUNCTION
+}
+
+func (lState *luaState) IsThread(idx int) bool {
+	return lState.Type(idx) == LUA_TTHREAD
+}
+
 // IsString 判断索引处是不是string或者数字类型
 func (lState *luaState) IsString(idx int) bool {
 	t := lState.Type(idx)
@@ -79,18 +91,6 @@ func (lState *luaState) IsInteger(idx int) bool {
 func (lState *luaState) ToBoolean(idx int) bool {
 	val := lState.stack.get(idx)
 	return convertToBoolean(val)
-}
-
-// convertToBoolean 转换某个值为bool
-func convertToBoolean(val luaValue) bool {
-	switch x := val.(type) {
-	case nil:
-		return false
-	case bool:
-		return x
-	default:
-		return true
-	}
 }
 
 // ToNumber 取出指定索引上的数字，如果索引上值不是数字，那么将指定索引上的值转换为数字。如果无法转换那么返回0
@@ -123,4 +123,27 @@ func (lState *luaState) ToIntegerX(idx int) (int64, bool) {
 	val := lState.stack.get(idx)
 	i, ok := val.(int64)
 	return i, ok
+}
+
+// ToString 获取指定索引上的string，如果不是string会试图转换成string，会修改栈上内容
+func (lState *luaState) ToString(idx int) string {
+	s, _ := lState.ToStringX(idx)
+	return s
+}
+
+// ToStringX 如果是string类型那么会直接返回，如果是数字那么会转换成字符串然后返回，如果不能转换那么会返回空字符串，并返回false代表转换失败
+// 注意会修改栈上的内容
+func (lState *luaState) ToStringX(idx int) (string, bool) {
+	val := lState.stack.get(idx)
+	switch x := val.(type) {
+	case string:
+		return x, true
+	case int64, float64:
+		s := fmt.Sprintf("%v", x)
+		// 这里会修改栈上的内容
+		lState.stack.set(idx, s)
+		return s, true
+	default:
+		return "", false
+	}
 }
